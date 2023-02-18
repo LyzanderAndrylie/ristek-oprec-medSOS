96319101b37cb38096beb100be857782761c17df
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-02-18 21:28:02 +0700

    Configure and restructure

--- medSOS/settings.py
+++ medSOS/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'information',
     'tailwind',
     'theme',
     'django_browser_reload',
+    'information',
 ]
 
 MIDDLEWARE = [
