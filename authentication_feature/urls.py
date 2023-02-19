0ba2ac373a0ce24de59020b022546b1a2a77bc68
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-02-19 23:20:47 +0700

    Implement login and logout

--- authentication_feature/urls.py
+++ authentication_feature/urls.py
@@ -5,5 +5,8 @@ app_name = 'authentication_feature'
 
 urlpatterns = [
     path('register/', register_page, name="register_page"),
-    path('register/ajax', register, name="register_ajax"),
+    path('register/ajax', register_ajax, name="register_ajax"),
+    path('login/', login_page, name="login_page"),
+    path('login/ajax', login_ajax, name="login_ajax"),
+    path('logout/', logout_ajax, name="logout_ajax"),
 ]
--- authentication_feature/views.py
+++ authentication_feature/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def register_page(request):
@@ -8,7 +9,8 @@ def register_page(request):
     context = {'form': form}
     return render(request, 'register.html', context)
 
-def register(request):
+
+def register_ajax(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -27,3 +29,39 @@ def register(request):
         "status": False,
         "message": "Register Fail"
     }, status=401)
+
+
+def login_page(request):
+    return render(request, 'login.html')
+
+
+def login_ajax(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return JsonResponse({
+                "status": True,
+                "message": "Successfully Logged In!"
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Failed to Login, Account Disabled."
+            }, status=401)
+
+    else:
+        return JsonResponse({
+            "status": False,
+            "message": "Failed to Login, check your email/password."
+        }, status=401)
+
+
+def logout_ajax(request):
+    logout(request)
+    return JsonResponse({
+        "status": True,
+        "message": "Successfully Logout In!"
+    }, status=200)
