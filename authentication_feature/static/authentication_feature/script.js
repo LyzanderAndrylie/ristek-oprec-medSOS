0b1cb6646ce3cbe4ae180f22e9494e0b58dab806
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2023-02-22 16:41:25 +0700

    Fix bug

--- authentication_feature/static/authentication_feature/script.js
+++ authentication_feature/static/authentication_feature/script.js
@@ -18,15 +18,24 @@ const form = document.querySelector("form");
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const response = await fetch(form?.dataset.posturl, {
-        method: "POST",
-        body: new FormData(form),
-    })
+    try {
+        const response = await fetch(form?.dataset.posturl, {
+            method: "POST",
+            body: new FormData(form),
+        })
+    
+        const data = await response.json();
+        console.log(data);
 
-    if (response.status) {
-        location.assign(form?.dataset.redirecturl);
-    } else {
-        alert(response?.message);
-        $("[name=csrfmiddlewaretoken]").removeAttr("disabled");
+        if (response.status === 200) {
+            location.assign(form?.dataset.redirecturl);
+        } else {
+            alert(data?.message);
+            $("[name=csrfmiddlewaretoken]").removeAttr("disabled");
+        }
+        
+    } catch (error) {
+        console.log(error);
     }
+
 })
